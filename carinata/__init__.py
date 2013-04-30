55d5aa1b1d85defb31b389ec78bbb081aaac1530
Scott McGinness <mcginness.s@gmail.com>
2013-04-30 10:13:11 +0100

    Use astor to get codegen module

--- carinata/__init__.py
+++ carinata/__init__.py
@@ -18,11 +18,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-import codegen
 import os
 import re
 import sys
 import unittest
+from astor import codegen
 
 
 try:
--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='carinata',
-      version='0.1.0',
+      version='0.1.2',
       description='A rough-scaled python spec runner',
       author='Scott McGinness',
       author_email='mcginness.s@gmail.com',
       url='https://github.com/scottmcginness/carinata',
       packages=['carinata'],
+      install_requires=['astor >= 0.2.1']
      )
